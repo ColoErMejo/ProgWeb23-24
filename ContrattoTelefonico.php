1cb0a954f59e1360fa64cce701cdcd08cea02304
4TK4 <145766850+4TK4@users.noreply.github.com>
2024-05-07 16:23:41 +0200

    Adesso funziona
    Manca solo il link nella colonna SIM Disattive

--- ContrattoTelefonico.php
+++ ContrattoTelefonico.php
@@ -107,40 +107,13 @@
 								echo "<p>DB Error on Query: " . $e->getMessage() . "</p>";
 								$error = true;
 							}
-<<<<<<< Updated upstream
+
 							if (!$error) {
 								foreach ($result as $riga) {
 									$NumeroTelefonate = $riga["NumeroTelefonate"];
 									?>
-									<td><a href=""><?php echo $NumeroTelefonate; ?></a></td> <?php
-=======
-							$Numero = $riga["Numero"];
-							$DataAttivazione = $riga["DataAttivazione"];
-							$Tipo = $riga["Tipo"];
-							$MinutiResidui = $riga["MinutiResidui"];
-							$CreditoResiduo = $riga["CreditoResiduo"];
-							?>
-								<tr <?php echo $classRiga; ?>>
-									<td> <?php echo $Numero; ?> </td>
-									<td> <?php echo $DataAttivazione; ?> </td>
-									<td> <?php echo $Tipo; ?> </td>
-									<td> <?php echo $MinutiResidui; ?> </td>
-									<td> <?php echo $CreditoResiduo; ?> </td>
-								<?php
-								
-								$query = getTelefonateContrattoQry($Numero);
-								try {
-									$result = $conn->query($query);
-								} catch (PDOException $e) {
-									echo "<p>DB Error on Query: " . $e->getMessage() . "</p>";
-									$error = true;
-								}
-									if (!$error) {
-										foreach($result as $riga){
-										$NumeroTelefonate = $riga["NumeroTelefonate"];
-										?> <td><a href="Telefonata.php?EffettuataDa=<?php echo $Numero ?>"><?php echo $NumeroTelefonate; ?></a></td> <?php
-									}
->>>>>>> Stashed changes
+									<td><a href="Telefonata.php?EffettuataDa=<?php echo $Numero?>"><?php echo $NumeroTelefonate; ?></a></td> <?php
+
 								}
 							}
 
@@ -179,26 +152,8 @@
 								}
 							}
 
-<<<<<<< Updated upstream
 							?>
 						</tr>
-=======
-								$query = getSIMDisattiveContrattoQry($Numero);
-								try {
-									$result = $conn->query($query);
-								} catch (PDOException $e) {
-									echo "<p>DB Error on Query: " . $e->getMessage() . "</p>";
-									$error = true;
-								}
-									if (!$error) {
-										foreach($result as $riga){
-										$NumeroSIMDisattive = $riga["NumeroSIMDisattive"];
-										?> <td><a href="SIM.php?StatoSIM=SIMDisattiva&Contratto=<?php echo $Numero ?>"><?php echo $NumeroTelefonate; ?></a></td> <?php
-									}
-								}
-
-								?></tr>
->>>>>>> Stashed changes
 
 
 					<?php } ?>
