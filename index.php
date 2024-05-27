70f70c1095da9dbfd0683d6e5b5ba2882dc698bc
Lorenzo Colombo <l.colombo34@studenti.unibg.it>
2024-05-27 14:18:03 +0200

    testo home (1)

--- index.php
+++ index.php
@@ -17,23 +17,23 @@
 	include 'DBManager.php';
 	include 'connectDB.php';
 	?>
-	<div class="container">
-		<div class="research-filter">
-			<!--<img src="icons\trapgif.gif" width="300px" alt="TrapGif">-->
-			<h3>Introduzione</h3>
-			<p>Questo gestionale è stato progettato per amministrare facilmente il database di una nota compagnia telefonica.
-				L'obiettivo è gestire tutti i contratti telefonici stipulati dalla compagnia, la relativa lista delle chiamate
-				effettuate da ogni numero e la totalità delle SIM. Ogni SIM può essere attiva, disattiva o non attiva.
-				L'utente può aggiungere nuovi contratti, specificando il numero di telefono, la data di attivazione,
-				la tipologia di contratto (a ricarica / a consumo) e gli eventuali credito/minuti rimanenti.
-				Si possono aggiornare i campi di ogni contratto telefonico ed eliminarne anche, in questo caso vengono
-				eliminate anche tutte le tuple del registro delle chiamate associate a quel numero e cancellate le SIM associate.</p>
-		</div>
+	<div class="container" id="containerHome">
 
-		<div class="content-results">
-		
-		</div>
+		<!--<img src="icons\trapgif.gif" width="300px" alt="TrapGif">-->
+		<h3>Introduzione</h3>
+		<p>Questo gestionale è stato progettato per amministrare facilmente il database di una nota compagnia
+			telefonica.
+			L'obiettivo è gestire tutti i contratti telefonici stipulati dalla compagnia, la relativa lista delle
+			chiamate
+			effettuate da ogni numero e la totalità delle SIM. <br> Ogni SIM può essere attiva, disattiva o non attiva.
+
+			L'utente può aggiungere nuovi contratti, specificando il numero di telefono, la data di attivazione,
+			la tipologia di contratto (a ricarica / a consumo) e gli eventuali credito/minuti rimanenti. <br>
+			Si possono aggiornare i campi di ogni contratto telefonico ed eliminarne anche, in questo caso vengono
+			eliminate anche tutte le tuple del registro delle chiamate associate a quel numero e cancellate le SIM
+			associate.</p>
 	</div>
+
 </body>
 
 </html>
